22af18f94ec31d66cc7e552c4e1d3a07c53942cc
Daniel Moura <danielmoura@MacBook-Pro.local>
2021-10-05 16:55:44 -0300

    Refactoring axios

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,6 +14,7 @@ interface Props {
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean
 }
 
 export default function ActivityDashboard({
@@ -25,7 +26,8 @@ export default function ActivityDashboard({
   openForm,
   closeForm,
   createOrEdit,
-  deleteActivity
+  deleteActivity,
+  submitting
 }: Props) {
   return (
     <Grid>
@@ -33,6 +35,7 @@ export default function ActivityDashboard({
         <ActivityList activities={activities} 
         selectActivity={selectActivity} 
         deleteActivity={deleteActivity}
+        submitting={submitting}
         />
       </Grid.Column>
 
@@ -49,6 +52,7 @@ export default function ActivityDashboard({
             closeForm={closeForm}
             activity={selectedActivity}
             createOrEdit={createOrEdit}
+            submitting={submitting}
           />
         )}
       </Grid.Column>
